2ce35e58c6dd4f60e6cb5155606fd210dfed7bb0
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2024-03-05 11:48:36 +0530

    projectList row updated

--- src/components/ProjectListRow.tsx
+++ src/components/ProjectListRow.tsx
@@ -1,30 +1,52 @@
 import { Link } from "react-router-dom";
 
 type ProjectProps = {
-  index:  number,
-  title: string,
-  domain: string,
-  guide: string
-}
+  index: number;
+  title: string;
+  domain: string;
+  guide: string;
+};
 
-export default function ProjectListRow({ celltype, project }: { celltype: String, project: ProjectProps }) {
+export default function ProjectListRow({
+  celltype,
+  project,
+}: {
+  celltype: string;
+  project: ProjectProps;
+}) {
   return (
     <div>
-      <Link to={celltype==="heading" ? '/' : `/project/${project.index}`}>
-        <div className={`flex items-start justify-start space-x-8 bg-[#FFFFFF] ${celltype==="heading" ? "font-semibold py-1 cursor-default": "py-2 text-slate-500"} px-5 rounded-sm`}>
+      <Link to={celltype === "heading" ? "/" : `/project/${project.index}`}>
+        <div
+          className={`flex items-start justify-start space-x-8 bg-[#FFFFFF] ${
+            celltype === "heading"
+              ? "font-semibold py-1 cursor-default"
+              : "py-2 text-slate-500"
+          } px-5 rounded-sm`}
+        >
           <div>
-            <div className={`flex justify-start w-[50vw] items-start space-x-8 ${celltype === "heading" && "font-semibold"}"`}>
-              <p>{celltype==="heading" ? "#" : project.index }</p>
-              <p className="line-clamp-1">{project.title || "untitled project"}</p>
+            <div
+              className={`flex justify-start w-[50vw] items-start space-x-8 ${
+                celltype === "heading" && "font-semibold"
+              }"`}
+            >
+              <p>{celltype === "heading" ? "#" : project.index}</p>
+              <p className="line-clamp-1">
+                {project.title || "untitled project"}
+              </p>
             </div>
           </div>
 
           <div>
             <div className="flex text-start items-start space-x-8">
-              <p className="w-[15vw] line-clamp-1">{project.domain || "unspecified"}</p>
-              <p className="w-[15vw] line-clamp-1">{project.guide || "unknown"}</p>
+              <p className="w-[15vw] line-clamp-1">
+                {project.domain || "unspecified"}
+              </p>
+              <p className="w-[15vw] line-clamp-1">
+                {project.guide || "unknown"}
+              </p>
             </div>
-            </div>
+          </div>
         </div>
       </Link>
     </div>
