cf30b4cc8e08808c5fda04dd2c75afbab35a8bd8
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2024-03-22 21:28:31 +0530

    changes 4

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,20 +1,27 @@
-import validator from 'validator';
+import validator from "validator";
+import defaultProfile from "../assets/images/default_profile.png";
 
-export default function Person({name, photo}: {name: string, photo: string}) {
-  const photoUrl = (photo && validator.isURL(photo))? photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png";
-  console.log(name, photo, photoUrl)
+export default function Person({
+  name,
+  photo,
+}: {
+  name: string;
+  photo: string;
+}) {
+  const photoUrl = photo && validator.isURL(photo) ? photo : defaultProfile;
+  console.log(name, photo, photoUrl);
   return (
     <div className="relative leading-tight px-14 md:px-0">
-        <div className="w-32 h-32 block bg-slate-400 rounded-full absolute top-2 blur-[2px]"></div>
-        <img 
-          className="max-w-32 max-h-32 bg-white border-white rounded-full border-2 drop-shadow-xl" 
-          src={photoUrl} 
-          alt="profile" 
-        />
-        <div className="mt-5">
-            <p className="font-semibold">D16AD</p>
-            <p className="text-sm opacity-60">{name}</p>
-        </div>
+      <div className="w-32 h-32 block bg-slate-400 rounded-full absolute top-2 blur-[2px]"></div>
+      <img
+        className="max-w-32 max-h-32 bg-white border-white rounded-full border-2 drop-shadow-xl"
+        src={photoUrl}
+        alt="profile"
+      />
+      <div className="mt-5">
+        <p className="font-semibold">D16AD</p>
+        <p className="text-sm opacity-60">{name}</p>
+      </div>
     </div>
-  )
+  );
 }
